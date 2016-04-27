49b4987511044a9f945489ad2abe89abda34b8b7
MoeMixMC <moabdalla246@gmail.com>
2016-04-27 09:55:43 -0600

    added Hangman.h

---
+++ Hangman.h
@@ -0,0 +1,31 @@
+
+#define HANGMAN_H_
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Hangman
+{
+    public:
+        Hangman();
+        virtual ~Hangman();
+        void addBodyPart();
+        string GenerateWord();
+        int randomNumber(int maxNum);
+        void letterCheck(string input);
+        void StringToArray();
+        bool Lost();
+        bool win();
+        void DisplayHangmanPicture();
+        bool WIN;
+        void WinMessage();
+        void printProgressBar();
+    private:
+        vector<string> wordArray;
+        vector<string> displayArray;
+        vector<string> guessedLetters;
+        int Tries;
+        string WORD;
+};
